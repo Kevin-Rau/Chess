d6520b3e1ee28993e44b45081a89a69cf1294ff1
picokol <serpicofossas@gmail.com>
2016-04-13 13:27:14 -0600

    update the Board & Piece class, especially the printing routines

--- chess.py
+++ chess.py
@@ -28,8 +28,14 @@ class Piece:
 	def setID(self,id):
 		self._id = id
 		
+	def getID(self):
+		return self._id
+		
 	def setColor(self,color):
 		self._color = color
+		
+	def getColor(self):
+		return self._color
 		
 	def setState(self,state):
 		self._state = state
@@ -49,22 +55,52 @@ class Piece:
 
 class Board:
 	 
-	_board = [[' 0 ' for x in range(8)] for x in range(8)]
+	_board = [[' ' for x in range(8)] for x in range(8)]
 	
 	def __init__(self):
+		self._board[0][0] = Piece('R',False)
+		self._board[0][1] = Piece('N',False)
+		self._board[0][2] = Piece('B',False)
+		self._board[0][3] = Piece('Q',False)
+		self._board[0][4] = Piece('K',False)
+		self._board[0][5] = Piece('B',False)
+		self._board[0][6] = Piece('N',False)
+		self._board[0][7] = Piece('R',False)
 		for i in range(8):
-			self._board[1][i] = " P "
-		
+			self._board[1][i] = Piece('P',False)
+		for i in range(8):
+			self._board[6][i] = Piece('P',True)
+		self._board[7][0] = Piece('R',True)
+		self._board[7][1] = Piece('N',True)
+		self._board[7][2] = Piece('B',True)
+		self._board[7][3] = Piece('Q',True)
+		self._board[7][4] = Piece('K',True)
+		self._board[7][5] = Piece('B',True)
+		self._board[7][6] = Piece('N',True)
+		self._board[7][7] = Piece('R',True)
+	
+	def printPiece(self,piece):
+		# string = blank square, object = piece
+		if(type(piece) is str):
+			print(" " + piece + " ",end="")
+		else:
+			if(piece.getColor()):
+				print(Back.RESET + Fore.RESET + " " + piece.getID() + " ",end="")
+			else:
+				print(Back.BLACK + Fore.WHITE +  " " + piece.getID() + " ",end="")
+				print(Back.RESET + Fore.RESET +  "",end="")
+	
 	def printBoard(self):
 		print("---------------------------------")
 		for i in range(8):
 			for j in range(8):
-				print("|" + Board._board[i][j],end="")
+				print("|",end="")
+				self.printPiece(Board._board[i][j])
 			print("|")
 			print("---------------------------------")
 			
 chess = Board()
 chess.printBoard()
 
-pawn = Piece('Q',True)
-pawn.printPiece()
+#pawn = Piece('Q',True)
+#pawn.printPiece()
