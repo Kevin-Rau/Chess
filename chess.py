067e3482f5918f69cdff1a90509858ebf9fb14e8
picokol <serpicofossas@gmail.com>
2016-04-13 13:42:42 -0600

    added Strategy Pattern, specifically the move types, moves still need to be coded though

--- chess.py
+++ chess.py
@@ -19,11 +19,13 @@ class Piece:
 	_id = ''
 	_color = False 
 	_state = False
+	_move = None
 	
-	def __init__(self,id,color):
+	def __init__(self,id,color,move):
 		self.setID(id)
 		self.setColor(color)
 		self.setState(1)
+		self.move = move
 		
 	def setID(self,id):
 		self._id = id
@@ -40,6 +42,16 @@ class Piece:
 	def setState(self,state):
 		self._state = state
 		
+	def getState(self):
+		return self._state
+	
+	# this will be needed for upgrading pawn, need to change id as well...
+	def setMove(self,move):
+		self.move = move
+	
+	def move(self):
+		pass
+		
 	# this is for debugging only!!
 	def printPiece(self):
 		if(self._color):
@@ -51,33 +63,53 @@ class Piece:
 		else:
 			s = 'dead'
 		print("id: " + self._id + " | color: " + c + " | state: " + s)
-	
+
+def King():
+	pass
+
+def Queen():
+	pass
+
+def Bishop():
+	pass
+
+def Knight():
+	pass
+
+def Rook():
+	pass
+
+def Pawn():
+	pass
 
 class Board:
 	 
+	 # board will store piece positions
+	 # death will store dead pieces
 	_board = [[' ' for x in range(8)] for x in range(8)]
+	_death = []
 	
 	def __init__(self):
-		self._board[0][0] = Piece('R',False)
-		self._board[0][1] = Piece('N',False)
-		self._board[0][2] = Piece('B',False)
-		self._board[0][3] = Piece('Q',False)
-		self._board[0][4] = Piece('K',False)
-		self._board[0][5] = Piece('B',False)
-		self._board[0][6] = Piece('N',False)
-		self._board[0][7] = Piece('R',False)
+		self._board[0][0] = Piece('R',False,Rook)
+		self._board[0][1] = Piece('N',False,Knight)
+		self._board[0][2] = Piece('B',False,Bishop)
+		self._board[0][3] = Piece('Q',False,Queen)
+		self._board[0][4] = Piece('K',False,King)
+		self._board[0][5] = Piece('B',False,Bishop)
+		self._board[0][6] = Piece('N',False,Knight)
+		self._board[0][7] = Piece('R',False,Rook)
 		for i in range(8):
-			self._board[1][i] = Piece('P',False)
+			self._board[1][i] = Piece('P',False,Pawn)
 		for i in range(8):
-			self._board[6][i] = Piece('P',True)
-		self._board[7][0] = Piece('R',True)
-		self._board[7][1] = Piece('N',True)
-		self._board[7][2] = Piece('B',True)
-		self._board[7][3] = Piece('Q',True)
-		self._board[7][4] = Piece('K',True)
-		self._board[7][5] = Piece('B',True)
-		self._board[7][6] = Piece('N',True)
-		self._board[7][7] = Piece('R',True)
+			self._board[6][i] = Piece('P',True,Pawn)
+		self._board[7][0] = Piece('R',True,Rook)
+		self._board[7][1] = Piece('N',True,Knight)
+		self._board[7][2] = Piece('B',True,Bishop)
+		self._board[7][3] = Piece('Q',True,Queen)
+		self._board[7][4] = Piece('K',True,King)
+		self._board[7][5] = Piece('B',True,Bishop)
+		self._board[7][6] = Piece('N',True,Knight)
+		self._board[7][7] = Piece('R',True,Rook)
 	
 	def printPiece(self,piece):
 		# string = blank square, object = piece
@@ -101,6 +133,3 @@ class Board:
 			
 chess = Board()
 chess.printBoard()
-
-#pawn = Piece('Q',True)
-#pawn.printPiece()
