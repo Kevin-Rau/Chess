105cb62484624b277fae4b13e1fcc5f54c8e61bc
picokol <serpicofossas@gmail.com>
2016-04-19 23:03:23 -0600

    added ridiculous fix for terrible python host resolution

--- chess.py
+++ chess.py
@@ -954,7 +954,7 @@ class Connect:
 		self._port = 8080
 
 	def hostGame(self):
-		self._host = socket.gethostbyname(socket.gethostname())
+		self._host = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 		self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self._socket.bind((self._host, self._port))
 		self._socket.listen(1)
