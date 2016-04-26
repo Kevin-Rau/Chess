4a2cc69c0599c13b1b5c1563f3f3fa526d74886d
Ryan Riley <ryan.b.riley@colorado.edu>
2016-04-25 21:14:12 -0600

    fixing pawn reanimation bug

--- chess.py
+++ chess.py
@@ -585,14 +585,14 @@ class Board:
 		# move the piece to the destination
 		self.setPiece(orig[0],orig[1],' ')
 		self.setPiece(dest[0],dest[1],piece)
-		if (piece.getID() == 'P' or piece.getID() == '\u265F') and any(deadpiece.getColor() == False for deadpiece in self._dead):
+		if (piece.getID() == 'P' or piece.getID() == '\u265F') and any(deadpiece.getColor() == False for deadpiece in self._dead) and dest[1] == 7:
 			trade = input('which piece would you like to reanimate?')
 			if any(deadpiece.getID() == trade and deadpiece.getColor() == False for deadpiece in self._dead):
 				for deadpiece in self._dead:
 					if deadpiece.getID() == trade and deadpiece.getColor() == False:
 						self._tradepiece = deadpiece	
 						self.setPiece(dest[0],dest[1], deadpiece)
-		if (piece.getID() == 'p' or piece.getID() == '\u2659') and any(deadpiece.getColor() == True for deadpiece in self._dead):
+		if (piece.getID() == 'p' or piece.getID() == '\u2659') and any(deadpiece.getColor() == True for deadpiece in self._dead) and dest[1] == 0:
 			trade = input('which piece would you like to reanimate?')
 			if any(deadpiece.getID() == trade and deadpiece.getColor() == True for deadpiece in self._dead):
 				for deadpiece in self._dead:
